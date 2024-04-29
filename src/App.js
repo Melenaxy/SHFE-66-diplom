c065b4fbb90cb12d84c0c0383f5c36998ec2b814
MelnikovaEYu <MelnikovaEYu@ufa.infteh.ru>
2024-04-30 02:22:47 +0500

    Реализована разметка раздела "О нас"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { About } from './components/About/About';
 import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <About />
+    </>
   );
 }
 
--- src/components/Header/HeaderNavigation.jsx
+++ src/components/Header/HeaderNavigation.jsx
@@ -6,7 +6,9 @@ export const HeaderNavigation = () => {
       <div className='logo'>Лого</div>
       <nav className='header-nav'>
         <ul className='nav-menu'>
-          <li className='nav-menu-item'>О нас</li>
+          <li className='nav-menu-item'>
+            <a href='#about'>О нас</a>
+          </li>
           <li className='nav-menu-item'>Как это работает</li>
           <li className='nav-menu-item'>Отзывы</li>
           <li className='nav-menu-item'>Контакты</li>
